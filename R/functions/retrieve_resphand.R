00ffcd61514ad2f677d2b993e2761b868e3176d3
marjoh <martin.johansson@donders.ru.nl>
2025-06-04 23:39:13 +0200

    IN PROGRESS: ap vs pp

--- R/functions/retrieve_resphand.R
+++ R/functions/retrieve_resphand.R
@@ -1,4 +1,4 @@
-retrieve_resphand <- function(filename='P:/3022026.01/pep/bids/derivatives/manipulated_merged_motor_task_mri_2023-09-15.csv'){
+retrieve_resphand <- function(filename='/project/3022026.01/pep/bids/derivatives/manipulated_merged_motor_task_mri_2023-09-15.csv'){
     
     df <- read_csv(filename, 
                    col_select = c('pseudonym', 'Timepoint','Group','RespondingHand')) %>%
